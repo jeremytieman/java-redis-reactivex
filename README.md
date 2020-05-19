729d134857c5c3df69a37bae4c6ff001b38150df
Jeremy Tieman <jeremy.tieman@disney.com>
2020-05-18 18:38:44 -0700

    Added Redis listener

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # java-redis-reactivex
 Test project in Java for Redis and ReactiveX
+
+# Redis
+
+This project was tested using the official Redis Docker image. In order to run the Docker image, first pull the image:
+
+`docker pull redis`
+
+Next, run the image:
+
+`docker run -it -p 6379:6379 --name redis1 -d redis`
--- src/main/java/com/codexmachina/javaredisreactivex/ListMessagesServlet.java
+++ src/main/java/com/codexmachina/javaredisreactivex/ListMessagesServlet.java
@@ -2,14 +2,26 @@ package com.codexmachina.javaredisreactivex;
 
 import java.io.IOException;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Jedis;
+
 public class ListMessagesServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -4751096228274971485L;
+
+	private final Map<String, List<String>> messages = new HashMap<>();
+	private final JedisPool jedisPool = new JedisPool();
+	private Jedis jedis = null;
+	private RedisPubSubListener listener = null;
 
 	@Override
 	protected void doGet(HttpServletRequest reqest, HttpServletResponse response) throws ServletException, IOException {
@@ -18,11 +30,31 @@ public class ListMessagesServlet extends HttpServlet {
 	
 	@Override
 	public void init() throws ServletException {
+		jedis = jedisPool.getResource();
+		listener = new RedisPubSubListener(messages, true);
+
+		new Thread(new Runnable() {
+		
+			@Override
+			public void run() {
+				try {
+					System.out.println("Subscribing to Redis...");
+					jedis.psubscribe(listener, "fc:*", "172.20.56.106:*");
+					jedis.close();
+					System.out.println("Subscribing to Redis succeeded.");
+				}
+				catch(Exception e) {
+					System.out.println("Subscribing to Redis failed");
+				}
+			}
+		}).start();
+
 		System.out.println("Servlet " + this.getServletName() + " has started");
 	}
 	
 	@Override
 	public void destroy() {
+		listener.close();
 		System.out.println("Servlet " + this.getServletName() + " has stopped");
 	}
 }
